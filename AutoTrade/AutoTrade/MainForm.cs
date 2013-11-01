8eddf160ac596ba95c4289385ffa45f4e016bd59
jacky Song <song_fuwei@126.com>
2013-11-01 18:52:34 +0800

    1

--- AutoTrade/AutoTrade/MainForm.cs
+++ AutoTrade/AutoTrade/MainForm.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.IO;
 using System.Threading;
 using AutoTrade.Core;
+using System.Web;
 
 namespace AutoTrade
 {
@@ -20,6 +21,15 @@ namespace AutoTrade
         Thread _thShow,_thInit;
         private string _Cookies = "";
 
+        static CookieContainer cookie2 = new CookieContainer();
+
+        public enum ResponeType
+        {
+            String,
+            File
+        }
+
+
         public MainForm()
         {
             InitializeComponent();
@@ -29,8 +39,8 @@ namespace AutoTrade
 
             CheckForIllegalCrossThreadCalls = false;//为false可以跨线程调用windows控件     
 
-            _thInit = new Thread(new ThreadStart(InitNet));
-            _thInit.Start();
+            //_thInit = new Thread(new ThreadStart(InitNet));
+            //_thInit.Start();
         }
 
         private void InitNet()
@@ -49,17 +59,36 @@ namespace AutoTrade
 
         private void button1_Click(object sender, EventArgs e)
         {
-            byte[] bs = new byte[16];
-            bs = Encoding.Default.GetBytes("Edifier1984");
-            Guid g = new Guid(bs);
-            MessageBox.Show(g.ToString());
+            string loginUrl = "https://passport.jd.com/new/login.aspx";
+            string s =  Get(loginUrl, null);
+            _ShowInfo.ShowInfo(s);
+            
         }
 
         private void bn_Login_Click(object sender, EventArgs e)
         {
-            _ShowInfo.Show();
-            _thShow = new Thread(new ThreadStart(Login));
-            _thShow.Start();           
+            //_ShowInfo.Show();
+            //_thShow = new Thread(new ThreadStart(Login));
+            //_thShow.Start();
+            string loginUrl = "https://passport.jd.com/new/login.aspx";
+            Get(loginUrl, null);
+
+            string loginRefererUrl = "http://passport.jd.com/uc/login?ltype=logout";
+            string loginServiceUrl = "http://passport.jd.com/uc/loginService";
+            var s = Post(loginServiceUrl,
+               new Dictionary<string, string>(){
+                {"uuid",Convert.ToString(Guid.NewGuid())},
+                {"loginname",HttpUtility.UrlEncode("flysnoopy1984")},
+                {"nloginpwd",HttpUtility.UrlEncode("Edifier1984")},
+                {"loginpwd",HttpUtility.UrlEncode("Edifier1984")},
+                {"machineNet","machineCpu"},
+                {"machineDisk",""},
+                {"authcode",""}}, loginRefererUrl);
+
+            var dic = new Dictionary<string, string>();
+            s = Get("http://order.jd.com/center/list.action?r=635133982534597500", null, fun: a => dic = a);
+
+            _ShowInfo.ShowInfo(s);
         }
 
 
@@ -106,10 +135,132 @@ namespace AutoTrade
             item.Header.Add("Accept-Encoding", "gzip, deflate");
             item.ContentType = "text/html";
             item.UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)";
+           // item.CookieCollection = cookie2;
             item.Cookie = _Cookies;
             result = helper.GetHtml(item);
           
             _ShowInfo.ShowInfo(result.Html);
+        }
+
+        public static string Post(string url, Dictionary<string, string> postData, string referer = "", string accept = "", string contentType = "", ResponeType type = ResponeType.String, string fileSavePath = "", Action<string> action = null, Func<Dictionary<string, string>> fun = null)
+        {
+            var result = "";
+            //var cookie = new CookieContainer();
+            StringBuilder strPostData = new StringBuilder();
+            if (postData != null)
+            {
+                postData.AsQueryable().ToList().ForEach(a =>
+                {
+                    strPostData.AppendFormat("{0}={1}&", a.Key, a.Value);
+                });
+            }
+            byte[] byteArray = Encoding.UTF8.GetBytes(strPostData.ToString().TrimEnd('&'));
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            webRequest.CookieContainer = cookie2;
+
+            webRequest.Method = "POST";
+            if (string.IsNullOrEmpty(accept))
+                webRequest.Accept = "application/json, text/javascript, */*;";
+            else
+                webRequest.Accept = accept;
+
+            if (!string.IsNullOrEmpty(referer))
+                webRequest.Referer = referer;
+            if (string.IsNullOrEmpty(contentType))
+                webRequest.ContentType = "application/x-www-form-urlencoded";
+            else
+                webRequest.ContentType = contentType;
+
+            if (strPostData.Length > 0)
+                webRequest.ContentLength = byteArray.Length;
+
+            //请求
+            Stream newStream = webRequest.GetRequestStream();
+            newStream.Write(byteArray, 0, byteArray.Length);
+            newStream.Close();
+
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+            var responSteam = response.GetResponseStream();
+
+            if (type == ResponeType.String)
+            {
+                StreamReader strRespon = new StreamReader(responSteam, Encoding.UTF8);
+                result = strRespon.ReadToEnd();
+            }
+            else
+            {
+                BinaryReader br = new BinaryReader(responSteam);
+                byte[] byteArr = br.ReadBytes(200000);
+                FileStream fs = new FileStream(fileSavePath, FileMode.OpenOrCreate);
+                fs.Write(byteArr, 0, byteArr.Length);
+                fs.Dispose();
+                fs.Close();
+                result = "OK";
+            }
+            if (action != null)
+            {
+                action.Invoke(result);
+            }
+            if (fun != null)
+            {
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                foreach (var item in cookie2.GetCookies(webRequest.RequestUri))
+                {
+                    var c = item as Cookie;
+                    dic.Add(c.Name, c.Value);
+                }
+                fun = () => { return dic; };
+            }
+            return result;
+
+        }
+
+
+        public static string Get(string url, Dictionary<string, string> postData = null, string referer = "", Action<string> action = null, Action<Dictionary<string, string>> fun = null)
+        {
+            var result = "";
+
+            StringBuilder strPostData = new StringBuilder("?");
+            if (postData != null)
+            {
+                postData.AsQueryable().ToList().ForEach(a =>
+                {
+                    strPostData.AppendFormat("{0}={1}&", a.Key, a.Value);
+                });
+            }
+            if (strPostData.Length == 1)
+                strPostData = strPostData.Clear();
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url + strPostData.ToString().TrimEnd('&'));
+            webRequest.CookieContainer = cookie2;
+            webRequest.Method = "GET";
+            webRequest.Accept = "text/javascript, text/html, application/xml, text/xml, */*;";
+            if (!string.IsNullOrEmpty(referer))
+                webRequest.Referer = referer;
+            //请求
+            HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
+            var responSteam = response.GetResponseStream();
+
+            StreamReader strRespon = new StreamReader(responSteam, Encoding.Default);
+            result = strRespon.ReadToEnd();
+
+            if (action != null)
+            {
+                action.Invoke(result);
+            }
+            if (fun != null)
+            {
+                Dictionary<string, string> dic = new Dictionary<string, string>();
+                foreach (var item in cookie2.GetCookies(webRequest.RequestUri))
+                {
+                    var c = item as Cookie;
+                    dic.Add(c.Name, c.Value);
+                }
+                fun.Invoke(dic);
+            }
+            return result;
+
         }
 
       
